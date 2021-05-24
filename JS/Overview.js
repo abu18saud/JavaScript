43481b31a8178113faea43c718069be85989a1ae
Abdullah Alsalem <abdullahsalem18@gmail.com>
2021-05-24 11:46:22 +0300

    Comments

---
+++ JS/Overview.js
@@ -0,0 +1,36 @@
+function welcome() {
+    document.write("Hello World!");
+}
+
+function outputToConsole() {
+    console.log("Hello from console!");
+}
+
+function YourFirstProgram() {
+    /*
+    Your First Program
+    Write a program to print "JS is fun".
+    Note that the sentence starts with capital letters.
+
+    Hint
+    Use console.log() function.
+
+    Remember to enclose the text into double quotes.
+    */
+    console.log("JS is fun");
+}
+
+function varibles() {
+    //It’s super important to remember that JavaScript variable names are case-sensitive.
+    var x = 100;
+    document.write(x);
+
+    var x = 100;
+    console.log(x);
+
+    x = 42;
+    console.log(x);
+}
+
+//function 
+
