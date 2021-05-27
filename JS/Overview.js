d83ced80b178d4c0287db0e5f3505866cc0f1848
Abdullah Alsalem <abdullahalsalem18@gmail.com>
2021-05-27 06:49:04 +0300

    Basic Concepts
    Basic Concepts

--- JS/Overview.js
+++ JS/Overview.js
@@ -1,5 +1,5 @@
 function welcome() {
-    document.write("Hello World!");
+    document.write("Hello World!\f");
 }
 
 function outputToConsole() {
@@ -32,5 +32,39 @@ function varibles() {
     console.log(x);
 }
 
-//function 
+function Comments() {
+    // This is a single line comment
+    alert("This is an alert box!");
+    /* This code 
+   creates an 
+   alert box */
+    alert("This is an alert box!");
+}
 
+function Even_numbers() {
+    //Change the code to output only the required numbers.
+    /*
+    The program you’re given outputs a list of names to the console.
+    Your mission, should you choose to accept it, is to change the code to output only the items with even numbers.
+    
+    Heads up!
+    You can uncomment or comment the corresponding lines in the code to result in the expected output.
+    */
+    //console.log("1. James")
+    console.log("2. John")
+    //console.log("3. Amy")
+    console.log("4. Collin")
+    //console.log("5. Tom")
+    console.log("6. Lisa")
+}
+
+function Escape() {
+//Escaping Characters in Strings
+//Use the escape function to generate the expected output.
+//Strings are enclosed in double or single quotes.
+//But what if we want to include double quotes in double quoted strings, or single quotes in single quoted strings? We need to escape!
+//Grab your lockpicks, and fix the code you’re given to generate the correct output.
+//Remember, you need to escape the quotes in the strings to fix the errors in the code.
+console.log("I'm learning JavaScript");
+console.log('A quote: "Java is to JavaScript as ham is to hamster."');
+}
