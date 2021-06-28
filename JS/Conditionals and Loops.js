48d76ad4ff52760d2c3fea7c2a16feaaf9c37e2c
Abdullah Alsalem <abdullahalsalem18@gmail.com>
2021-06-28 21:02:33 +0300

    while loop

--- JS/Conditionals and Loops.js
+++ JS/Conditionals and Loops.js
@@ -69,5 +69,117 @@ function AdmissionInPairs() {
 function BuyMore_GetMore() {
     var totalPrice = parseInt(readLine(), 10)
     // Your code here
+    if (totalPrice <= 999) {
+        console.log("0%");
+    } else if (totalPrice <= 2999) {
+        console.log("10%");
+    } else if (totalPrice <= 4999) {
+        console.log("30%");
+    } else {
+        console.log("50%");
+    }
+}
+/**Dark Theme
+ * Complete the program to output the theme according to input number.
+ * Switch statements
+ * The user can choose the color theme for the browser:
+ * 1. Light
+ * 2. Dark
+ * 3. Nocturne
+ * 4. Terminal
+ * 5. Indigo
+ * 
+ * You are given a program that takes
+ * the number as input. Complete the program so that it will output to the console the theme according to input number.
+ * 
+ * Sample Input
+ * 2
+ * 
+ * Sample Output
+ * Dark
+ * 
+ * Don't forget to use break for each case statement.
+ */
 
+function DarkTheme() {
+    var themeNumber = 1;
+    /*
+    1 - Light
+    2 - Dark
+    3 - Nocturne
+    4 - Terminal
+    5 - Indigo
+    */
+    // Your code here
+    switch (themeNumber) {
+        case 1:
+            console.log("Light");
+            break;
+        case 2:
+            console.log("Dark");
+            break;
+        case 3:
+            console.log("Nocturne");
+            break;
+        case 4:
+            console.log("Terminal");
+            break;
+        case 5:
+            console.log("Indigo");
+            break;
+    }
+}
+/**Pages Per Minute
+ * Write a program tto define and output the speed of the printer.
+ * The For loop
+ * The printer prints 20 pages per minute.
+ * Write a program that will output to the console how many pages will be printed each minute over the course of 6 minutes.
+ * 
+ * Output
+20
+40
+60
+80
+100
+120
+ * 
+ * Don't forget to increment the counter (i++) for every iteration.
+ */
+function PagesPerMinute() {
+    // Your code here
+    for (i = 1, dep = 20; i <= 6; i++) {
+        result = dep * i;
+        console.log(result);
+    }
+}
+/** Factorial Fun
+ * Write a program to output to the console the factorial of ...
+ * The for loop
+ * A number's factorial is the product of all positive integers less than or equal to the number.
+ * Write a program that takes a number as input and outputs its factorial to the console.
+ * 
+ * Sample Input
+ * 5
+ * 
+ * Sample Output
+ * 120
+ * 
+ * Explanation
+ * 5*4*3*2*1 = 120
+ * 
+ * A factorial variable is declared for initial iteration.
+ */
+function factorialFun() {
+    // Your code here
+    var number = parseInt(readLine(), 10)
+    var factorial = 1;
+
+    if (number == 0 || number == 1) {
+        console.log(factorial);
+    } else {
+        for (var i = number; i >= 1; i--) {
+            factorial = factorial * i;
+        }
+        console.log(factorial);
+    }
 }
